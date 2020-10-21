ef7a20a20c1e1e08135eb7313336fdbf6c47c61b
Cel Pynenborg <cel.pynenborg@healthconnect.be>
2020-10-21 17:30:43 +0200

    initial commit

---
+++ src/main/java/com/livelypower/elasticsearch/restclient/ElasticsearchRestclientApplication.java
@@ -0,0 +1,13 @@
+package com.livelypower.elasticsearch.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchRestclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchRestclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livelypower/elasticsearch/restclient/ElasticsearchRestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livelypower.elasticsearch.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchRestclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
